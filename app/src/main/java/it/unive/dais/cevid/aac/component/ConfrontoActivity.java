c5e3ed10987626bde5d47c15717d194978bc889b
Gianmarco Callegher <854612@stud.unive.it>
2018-02-20 19:00:43 +0100

    Started Managing multiple Parser in ConfrontoActivity

--- app/src/main/java/it/unive/dais/cevid/aac/component/ConfrontoActivity.java
+++ app/src/main/java/it/unive/dais/cevid/aac/component/ConfrontoActivity.java
@@ -1,5 +1,6 @@
 package it.unive.dais.cevid.aac.component;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -10,11 +11,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import it.unive.dais.cevid.aac.R;
 import it.unive.dais.cevid.aac.fragment.FragmentAdapter;
+import it.unive.dais.cevid.aac.item.AbstractItem;
 import it.unive.dais.cevid.aac.item.MunicipalityItem;
 import it.unive.dais.cevid.aac.item.UniversityItem;
+import it.unive.dais.cevid.datadroid.lib.parser.AppaltiParser;
+import it.unive.dais.cevid.datadroid.lib.parser.SoldipubbliciParser;
+import it.unive.dais.cevid.datadroid.lib.parser.progress.ProgressBarManager;
 
 /**
  * Created by gianmarcocallegher on 12/02/2018.
@@ -27,6 +33,8 @@ public class ConfrontoActivity extends AppCompatActivity {
     private Map positionTitleMap;
     private String mode;
     private FragmentAdapter fragmentAdapter;
+    private Map<String, SoldipubbliciParser> codiceEnteSoldiPubbliciParserMap;
+    private Map<String, AppaltiParser> codiceEnteAppaltiParserMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +51,23 @@ public class ConfrontoActivity extends AppCompatActivity {
         else {
             l = new ArrayList<MunicipalityItem>();
             l.addAll((List<MunicipalityItem>) sl);
+        }
+
+        codiceEnteSoldiPubbliciParserMap = new HashMap<>();
+        codiceEnteAppaltiParserMap = new HashMap<>();
+
+        for (Object x : l) {
+            SoldipubbliciParser soldipubbliciParser =
+                    new SoldipubbliciParser(((AbstractItem) x).getId(), "UNI", null);
+
+            AppaltiParser appaltiParser = new AppaltiParser(((AbstractItem) x).getUrls(), null);
+
+            codiceEnteSoldiPubbliciParserMap.put(((AbstractItem) x).getId(), soldipubbliciParser);
+            codiceEnteAppaltiParserMap.put(((AbstractItem) x).getId(), appaltiParser);
+
+            soldipubbliciParser.getAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+            appaltiParser.getAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
         }
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
--- app/src/main/java/it/unive/dais/cevid/aac/fragment/TabFragment.java
+++ app/src/main/java/it/unive/dais/cevid/aac/fragment/TabFragment.java
@@ -18,6 +18,7 @@ import it.unive.dais.cevid.aac.adapter.AppaltiAdapter;
 import it.unive.dais.cevid.aac.adapter.SoldiPubbliciAdapter;
 import it.unive.dais.cevid.aac.util.EntitieExpenditure;
 import it.unive.dais.cevid.datadroid.lib.parser.AppaltiParser;
+import it.unive.dais.cevid.datadroid.lib.parser.SoldipubbliciParser;
 
 /**
  * Created by gianmarcocallegher on 19/02/2018.
@@ -49,11 +50,62 @@ public class TabFragment extends Fragment {
 
         List eel = new ArrayList<EntitieExpenditure>();
 
+        //prova soldipubblici
+
+        SoldipubbliciParser.Data sppd = new SoldipubbliciParser.Data();
+
+        sppd.descrizione_codice = "desc";
+        sppd.codice_siope = "13";
+        sppd.anno = "2018";
+        sppd.codice_gestionale = "AAA";
+        sppd.idtable = "true";
+        sppd.data_di_fine_validita = "2019";
+        sppd.cod_ente = "codente";
+        sppd.imp_uscite_att = "1.0";
+        sppd.periodo = "anni";
+        sppd.ricerca = "ricerca";
+        sppd.descrizione_ente = "descente";
+        sppd.importo_2017 = "10";
+        sppd.importo_2016 = "10";
+        sppd.importo_2015 = "10";
+        sppd.importo_2014 = "10";
+        sppd.importo_2013 = "10";
+
+        List<SoldipubbliciParser.Data> l1 = new ArrayList<SoldipubbliciParser.Data>();
+
+        l1.add(sppd);
+
+        for (SoldipubbliciParser.Data x : l1)
+            eel.add(new EntitieExpenditure(x, "2016"));
+
+        //fine prova soldipubblici
+
+        //prova appalti
+
+        AppaltiParser.Data apd = new AppaltiParser.Data();
+
+        apd.cig = "14";
+        apd.proponente = "proponente";
+        apd.codiceFiscaleProp = "cfprop";
+        apd.oggetto = "ogg";
+        apd.sceltac = "sceltac";
+        apd.aggiudicatario = "agg";
+        apd.codiceFiscaleAgg = "cfagg";
+        apd.importo = "10";
+        apd.importoSommeLiquidate = "10";
+        apd.dataInizio = "inizio";
+        apd.dataFine = "fine";
+
+        List<AppaltiParser.Data> l2 = new ArrayList<>();
+        l2.add(apd);
+
+        //fine prova appalti
+
         SoldiPubbliciAdapter soldiPubbliciAdapter = new SoldiPubbliciAdapter(eel, "1");
         recyclerViewExpenditure.setAdapter(soldiPubbliciAdapter);
         recyclerViewExpenditure.setVisibility(View.VISIBLE);
 
-        AppaltiAdapter appaltiAdapter = new AppaltiAdapter(new ArrayList<AppaltiParser.Data>(), 1.0);
+        AppaltiAdapter appaltiAdapter = new AppaltiAdapter(l2, 1.0);
         recyclerViewTenders.setAdapter(appaltiAdapter);
         recyclerViewTenders.setVisibility(View.VISIBLE);
 
@@ -65,4 +117,39 @@ public class TabFragment extends Fragment {
 
         return view;
     }
+
+    /*public static class Data implements Serializable {
+        public String descrizione_codice;
+        public String codice_siope;
+        public String descrizione_ente;
+        public String ricerca;
+        public String idtable;
+        public String cod_ente;
+        public String anno;
+        public String periodo;
+        public String codice_gestionale;
+        public String imp_uscite_att;
+        public String data_di_fine_validita;
+        public String importo_2013;
+        public String importo_2014;
+        public String importo_2015;
+        public String importo_2016;
+        public String importo_2017;
+
+    }*/
+
+    /*public static class Data implements Serializable{
+        public String cig;
+        public String proponente;
+        public String codiceFiscaleProp;
+        public String oggetto;
+        public String sceltac;
+        public String aggiudicatario;
+        public String codiceFiscaleAgg;
+        public String importo;
+        public String importoSommeLiquidate;
+        public String dataInizio;
+        public String dataFine;
+    }*/
+
 }
