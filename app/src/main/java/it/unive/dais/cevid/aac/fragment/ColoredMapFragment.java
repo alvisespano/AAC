538f2f6b694cc7ab052b79e2f243ba8b968df1f1
afericean <angelko.fericean@gmail.com>
2018-04-24 14:19:11 +0200

    Full Colored Map and JSON coord file

--- app/src/main/java/it/unive/dais/cevid/aac/fragment/ColoredMapFragment.java
+++ app/src/main/java/it/unive/dais/cevid/aac/fragment/ColoredMapFragment.java
@@ -31,15 +31,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Random;
 
 import it.unive.dais.cevid.aac.R;
-import it.unive.dais.cevid.aac.component.ColoredMapActivity;
+
 
 
 public class ColoredMapFragment extends Fragment implements OnMapReadyCallback{
     private static final String TAG = "ColoredMapFragment";
 
     protected GoogleMap mMap;
+
+    private ArrayList colorArray = new ArrayList();
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -86,10 +89,10 @@ public class ColoredMapFragment extends Fragment implements OnMapReadyCallback{
 
         try {
             Log.d(TAG, "Angelko ++");
-            InputStream in = getResources().openRawResource(R.raw.veneto);
-            String inputVeneto = convertStreamToString(in);
-            JSONObject reader = new JSONObject(inputVeneto);
-            coordinates = reader.getJSONArray("Veneto");
+            InputStream in = getResources().openRawResource(R.raw.coord_regions);
+            String input = convertStreamToString(in);
+            JSONObject reader = new JSONObject(input);
+            coordinates = reader.getJSONArray("coordinates");
             Log.d("Angelko ", String.valueOf(coordinates));
 
         } catch (JSONException e) {
@@ -100,18 +103,12 @@ public class ColoredMapFragment extends Fragment implements OnMapReadyCallback{
         return coordinates;
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        this.mMap = googleMap;
-
-
-        LatLng rome = new LatLng(41.89, 12.51);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(rome, 5));
-
-        JSONArray coordinates = this.parseCoordinates();
-
-        // Instantiates a new Polygon object and adds points to define a rectangle
+    private PolygonOptions createPolygon(JSONArray coord, int color)
+    {
+        JSONArray coordinates = coord;
         PolygonOptions rectOptions = new PolygonOptions();
+        int fillColor = 0x7F000000 + color;
+        int strokeColor = 0xFF000000 + color;
 
         for(int i=0;i<coordinates.length();i++)
         {
@@ -128,9 +125,51 @@ public class ColoredMapFragment extends Fragment implements OnMapReadyCallback{
 
         }
 
-        rectOptions.fillColor(Color.BLUE);
-        // Get back the mutable Polygon
-        mMap.addPolygon(rectOptions);
+        rectOptions.fillColor(fillColor);
+        rectOptions.strokeColor(strokeColor);
+        rectOptions.strokeWidth(8);
+
+        return rectOptions;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.mMap = googleMap;
+
+        colorArray.add(0x00FF00);
+        colorArray.add(0xFF0000);
+        colorArray.add(0x0000FF);
+        colorArray.add(0xFFFF00);
+        colorArray.add(0x00FFFF);
+        colorArray.add(0xFF00FF);
+
+        LatLng rome = new LatLng(41.89, 12.51);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(rome, 5.3F));
+
+        JSONArray allRegionsCoord = this.parseCoordinates();
+
+        for(int i=0;i<allRegionsCoord.length();i++)
+        {
+
+            try {
+                JSONArray region= (JSONArray) allRegionsCoord.get(i);
+                Random r = new Random();
+                int die = r.nextInt(6);
+                int col = (int) colorArray.get(die);
+
+                PolygonOptions rectOptions = this.createPolygon(region, col);
+
+                // Get back the mutable Polygon
+                mMap.addPolygon(rectOptions);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+
     }
 
 }
